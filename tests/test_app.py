14f274b9a8cb173164204e3c76be0f9ac655a121
Pedro Paiva <ph29@outlook.com.br>
2026-01-11 12:47:24 -0300

    learn: test craetad

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,11 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     response = client('/')
     assert response.status_code == HTTPStatus.OK
+
+
+def test_root_retorna_hello_em_html():
+    client = TestClient(app)
+
+    response = client('hello')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
